faf5f737d7ba65c1e2ad78314dfb734adf7271c9
Denis Senkov <defendercryak@gmail.com>
2022-05-10 15:26:12 +0300

    Добавьте файлы проекта.

---
+++ Task 4(4)/Task 4(4).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x = 0, y = 0, t = 0, a = 0;
+
+    cout << "\na = ";
+    cin >> a;
+
+    cout << "\nx = ";
+    cin >> x;
+
+    if (a <= x and (x + a) > 0)
+    {
+        y = a + log(x + a);
+    }
+
+    else if (a > x and sin(a * x) >= 0)
+    {
+        y = sqrt(sin(a * x));
+    }
+
+    if (a > y and (a - x) != 0)
+    {
+        t = y / (a - x);
+    }
+
+    else if (a == y and (a - x) != 0 and y != 0)
+    {
+        t = y / (a - x) + (a + x) / (y * y);
+    }
+
+    else if (a < y)
+    {
+        t = tan(a + x) + cos(2 * a * y);
+    }
+
+    cout << "\nt = " << t << "\n\ny = " << y << endl;
+}
